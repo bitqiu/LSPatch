2a33a424489f2a136759e7e516af7c9712c7d51a
Bit Qiu <i@bitqiu.cc>
2023-03-24 23:52:10 +0800

    Update Constants.java

--- share/java/src/main/java/org/lsposed/lspatch/share/Constants.java
+++ share/java/src/main/java/org/lsposed/lspatch/share/Constants.java
@@ -2,13 +2,13 @@ package org.lsposed.lspatch.share;
 
 public class Constants {
 
-    final static public String CONFIG_ASSET_PATH = "assets/lspatch/config.json";
-    final static public String LOADER_DEX_ASSET_PATH = "assets/lspatch/loader.dex";
-    final static public String META_LOADER_DEX_ASSET_PATH = "assets/lspatch/metaloader.dex";
-    final static public String ORIGINAL_APK_ASSET_PATH = "assets/lspatch/origin.apk";
-    final static public String EMBEDDED_MODULES_ASSET_PATH = "assets/lspatch/modules/";
+    final static public String CONFIG_ASSET_PATH = "assets/luban/config.json";
+    final static public String LOADER_DEX_ASSET_PATH = "assets/luban/loader.dex";
+    final static public String META_LOADER_DEX_ASSET_PATH = "assets/luban/metaloader.dex";
+    final static public String ORIGINAL_APK_ASSET_PATH = "assets/luban/origin.apk";
+    final static public String EMBEDDED_MODULES_ASSET_PATH = "assets/luban/modules/";
 
-    final static public String PATCH_FILE_SUFFIX = "-lspatched.apk";
+    final static public String PATCH_FILE_SUFFIX = "-lubaned.apk";
     final static public String PROXY_APP_COMPONENT_FACTORY = "org.lsposed.lspatch.metaloader.LSPAppComponentFactoryStub";
     final static public String MANAGER_PACKAGE_NAME = "org.lsposed.lspatch";
     final static public int MIN_ROLLING_VERSION_CODE = 348;
